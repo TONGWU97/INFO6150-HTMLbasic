ae0d6d89bfbce74c1d00e80a0d0c1f10458c4cd7
TONGWU97 <wu.to@husky.neu.edu>
2019-01-30 20:01:07 -0800

    Assignment 3

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component {
             <
             blockquote >
             <
-            p > The Statue of Liberty 's original torch is getting a new home.</p>
+            base target = "blank" / >
+            <
+            p > The < a href = "https://en.wikipedia.org/wiki/Statue_of_Liberty" > Statue of Liberty < /a> 's original torch is getting a new home.</p >
 
             <
             p > Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in 1984. It 's been stored in the statue'
@@ -30,14 +32,14 @@ class App extends Component {
             p > But visitors will be able to see the original 3, 600 - pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in May 2019. The torch was transported there by truck last week, along with a replica model of Lady Liberty 's face.</p>
 
             <
-            p > "Although it is not one of the most difficult things we have ever moved,"
-            says Douglas Phelps, who oversaw the relocation of the torch, "it is certainly the most important." < /p>
+            p > < em > "Although it is not one of the most difficult things we have ever moved," < /em>
+            says Douglas Phelps, who oversaw the relocation of the torch, < em > "it is certainly the most important." < /em> < /p >
 
             <
-            p > Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of "Outstanding Universal Value." < /p>
+            p > Not only is the Statue of Liberty the symbol of New York City, she is also a < a href = "https://whc.unesco.org/en/list/307" > UNESCO World Heritage site < /a> under the designation of < strong > "Outstanding Universal Value." < /strong > < /p >
 
             <
-            p > Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel--yes, the same guy who built Paris 's Eiffel Tower.</p>
+            p > Her full name is Liberty Enlightening the World, and she was designed by < a href = "https://en.wikipedia.org/wiki/Fr%C3%A9d%C3%A9ric_Auguste_Bartholdi" > Frederic Auguste Bartholdi < /a> and built by <a href="https:/ / en.wikipedia.org / wiki / Gustave_Eiffel ">Gustave Eiffel</a>--yes, the same guy who built Paris 's Eiffel Tower.</p >
 
             <
             p > Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation 's independence -- although, somewhat awkwardly, she arrived a decade too late.</p> <
@@ -48,23 +50,23 @@ class App extends Component {
             for many people who entered the United States as immigrants via neighboring Ellis Island. < /p>
 
             <
-            p > "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy,"
-            says John Piltzecker, superintendent of the Statue of Liberty National Monument.That symbolic spirit was bolstered by the decision to inscribe Emma Lazarus ' poem "The New Colossus" on the statue'
+            p > < em > "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," < /em>
+            says John Piltzecker, superintendent of the Statue of Liberty National Monument.That symbolic spirit was bolstered by the decision to inscribe Emma Lazarus ' poem <a href="https://www.poetryfoundation.org/poems/46550/the-new-colossus">"The New Colossus"</a> on the statue'
             s pedestal, which gave Lady Liberty one of her nicknames, "Mother of Exiles." < /p>
 
             <
-            p > The most famous lines in the poem are: "Give me your tired, your poor / Your huddled masses yearning to breathe free." < /p>
+            p > The most famous lines in the poem are: < em > "Give me your tired, your poor / Your huddled masses yearning to breathe free." < /em> < /p >
 
             <
-            p > In 2017, nearly 4.5 million people visited Liberty Island. < /p>
+            p > In 2017, nearly < strong > 4.5 million < /strong> people visited Liberty Island. < /p >
 
             <
-            p > When completed, the Statue of Liberty Museum will be 26, 000 square feet of history, inspiration and deeper analysis of the statue 's symbolism both in the US and around the world.</p>
+            p > When completed, the Statue of Liberty Museum will be < strong > 26, 000 square feet < /strong> of history, inspiration and deeper analysis of the statue 's symbolism both in the US and around the world.</p >
 
             <
             p > The "inspiration gallery,"
-            which is where the torch will live, will have floor - to - ceiling glass windows looking out at Lower Manhattan, including the World Trade Center. < /p>  <
-            /blockquote> <
+            which is where the torch will live, will have floor - to - ceiling glass windows looking out at Lower Manhattan, including the World Trade Center. < /p>  < /
+            blockquote > <
             /
             Article > <
             /div>
